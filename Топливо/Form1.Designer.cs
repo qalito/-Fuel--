32e458f4baa52b4deeb90d6af6940892488374e4
tosya <tosya3333@gmail.com>
2019-11-17 17:36:04 +0300

    Добавьте файлы проекта.

---
+++ Топливо/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Топливо
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(568, 40);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 16);
+            this.label6.TabIndex = 36;
+            this.label6.Text = "0,85";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(531, 40);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 16);
+            this.label7.TabIndex = 35;
+            this.label7.Text = "1,59";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(419, 40);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 16);
+            this.label8.TabIndex = 34;
+            this.label8.Text = "1,48";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(482, 40);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(32, 16);
+            this.label9.TabIndex = 33;
+            this.label9.Text = "1,59";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(567, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 19);
+            this.label5.TabIndex = 32;
+            this.label5.Text = "Газ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(533, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 19);
+            this.label4.TabIndex = 31;
+            this.label4.Text = "ДТ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(470, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 19);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "АИ-95";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(407, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 19);
+            this.label2.TabIndex = 29;
+            this.label2.Text = "АИ-92";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(-1, 245);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(624, 80);
+            this.label1.TabIndex = 28;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "АИ-92",
+            "АИ-95",
+            "ДТ",
+            "Газ"});
+            this.comboBox1.Location = new System.Drawing.Point(463, 120);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(150, 21);
+            this.comboBox1.TabIndex = 25;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.comboBox1_KeyDown);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.ForeColor = System.Drawing.Color.Coral;
+            this.checkBox1.Location = new System.Drawing.Point(107, 201);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 27;
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Indigo;
+            this.button1.Location = new System.Drawing.Point(463, 200);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 32);
+            this.button1.TabIndex = 26;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.textBox2.Location = new System.Drawing.Point(296, 147);
+            this.textBox2.MaxLength = 4;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ShortcutsEnabled = false;
+            this.textBox2.Size = new System.Drawing.Size(158, 20);
+            this.textBox2.TabIndex = 24;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.textBox1.Location = new System.Drawing.Point(296, 97);
+            this.textBox1.MaxLength = 10;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ShortcutsEnabled = false;
+            this.textBox1.Size = new System.Drawing.Size(158, 20);
+            this.textBox1.TabIndex = 23;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(624, 322);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Расчёт топлива";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ Топливо/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Топливо
+{
+    public partial class Form1 : Form
+    {
+        int vid = 0;
+        double Ras = 1;
+        double Sto = 1;
+        double Rash = 1;
+        bool Ch = false;
+        public bool allax = false;
+        public string halay;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = vid;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            try
+            {
+                Rash = Convert.ToDouble(textBox2.Text.Replace(".", ","));
+                textBox2.Text = textBox2.Text.Replace(".", ",");
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            try
+            {
+                Rash = Convert.ToDouble(textBox2.Text.Replace(" ", ""));
+                textBox2.Text = textBox2.Text.Replace(" ", "");
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            try
+            {
+                while ((textBox1.Text[textBox1.Text.Length - 1] == ',') || (textBox1.Text[textBox1.Text.Length - 1] == '.'))
+                {
+
+                    textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+                }
+                Ras = Convert.ToDouble(textBox1.Text.Remove(textBox1.Text.Length - 1));
+            }
+            catch (Exception ex)
+            {
+
+            }
+            try
+            {
+                while ((textBox2.Text[textBox2.Text.Length - 1] == ',') || (textBox2.Text[textBox2.Text.Length - 1] == '.'))
+                {
+
+                    textBox2.Text = textBox2.Text.Remove(textBox2.Text.Length - 1);
+
+
+                }
+                Rash = Convert.ToDouble(textBox2.Text.Remove(textBox2.Text.Length - 1));
+            }
+            catch (Exception ex)
+            {
+
+            }
+            try
+            {
+                while ((textBox2.Text[0] == '0') && (textBox2.Text.Length > 1) && (textBox2.Text[1] != ',') && (textBox2.Text[1] != '.'))
+                {
+                    Rash = Convert.ToDouble(textBox2.Text.Substring(1));
+                    textBox2.Text = textBox2.Text.Substring(1);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            try
+            {
+                while ((textBox1.Text[0] == '0') && (textBox1.Text.Length > 1) && (textBox1.Text[1] != ',') && (textBox1.Text[1] != '.'))
+
+                {
+                    Ras = Convert.ToDouble(textBox1.Text.Substring(1));
+                    textBox1.Text = textBox1.Text.Substring(1);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            try
+            {
+                Ras = Convert.ToDouble(textBox1.Text.Replace(".", ","));
+                textBox1.Text = textBox1.Text.Replace(".", ",");
+            }
+            catch (Exception ex)
+            {
+
+            }
+            try
+            {
+                Ras = Convert.ToDouble(textBox1.Text.Replace(" ", ""));
+                textBox1.Text = textBox1.Text.Replace(" ", "");
+            }
+            catch (Exception ex)
+            {
+
+            }
+            bool f = false;
+            allax = true;
+            try
+            {
+                Rash = Convert.ToDouble(textBox2.Text);
+                Ras = Convert.ToDouble(textBox1.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Некорректно введённые данные!\nВсе поля должны быть корректно заполнены!\nПовторите попытку!", "Ошибка!", MessageBoxButtons.OK);
+
+
+                checkBox1.Checked = false;
+                comboBox1.SelectedIndex = 0;
+                allax = false;
+
+            }
+
+            if ((Ras == 0) || (Rash == 0) || (Ras < 0) || (Rash < 0)) { MessageBox.Show("Неккоректно введённые данные!\n Введённые значения должны быть больше 0!", "Ошибка!", MessageBoxButtons.OK); }
+            else if (allax == true)
+            {
+                int tudaobr = 1;
+                if (checkBox1.Checked) { tudaobr = 2; }
+                if (((Ras / 100) * Sto * Rash * tudaobr) > 0.01)
+                {
+                    halay = Convert.ToString(Math.Round(((Ras / 100) * Sto * Rash * tudaobr), 2));
+                }
+                else halay = Convert.ToString(((Ras / 100) * Sto * Rash * tudaobr));
+
+                label1.Text = "Необходимое сумма для совершения поездки - " + halay + "BYN";
+
+            }
+
+
+
+
+            try
+            {
+                while ((textBox2.Text[0] == ',') && (Convert.ToInt32(textBox2.Text[1]) >= 0) || (Convert.ToInt32(textBox2.Text[1]) <= 0))
+                {
+                    textBox2.Text = "0" + textBox2.Text;
+                    Rash = Convert.ToDouble("0" + textBox2.Text);
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            try
+            {
+                while ((textBox1.Text[0] == ',') && (Convert.ToInt32(textBox1.Text[1]) >= 0) || (Convert.ToInt32(textBox1.Text[1]) <= 0))
+                {
+                    textBox1.Text = "0" + textBox1.Text;
+                    Ras = Convert.ToDouble("0" + textBox1.Text);
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    {
+                        vid = 0; Sto = 1.48;
+
+                    }
+                    break;
+
+                case 1:
+                    {
+                        vid = 1; Sto = 1.59;
+                    }
+                    break;
+
+                case 2:
+                    {
+                        vid = 2; Sto = 1.59;
+                    }
+                    break;
+
+                case 3:
+                    {
+                        vid = 3; Sto = 0.85;
+                    }
+                    break;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                textBox2.Focus();
+            }
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                comboBox1.Focus();
+            }
+        }
+
+        private void comboBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                checkBox1.Focus();
+            }
+        }
+
+        private void checkBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1.Focus();
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 8 && number != 44 && number != 46)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 8 && number != 44 && number != 46)
+            {
+                e.Handled = true;
+            }
+
+        }
+    }
+}
